0662edc4b64f9c7709bd3662f7266d9c3d85a6a3
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2023-12-25 18:09:51 +0300

    ed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Header/>
     </>
   )
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,47 @@
+//Компонент Navbar
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+// import logo from './typescript.png'
+
+const Header = () => {
+    return (
+        <>
+        <Navbar fixed="top" expand="md" bg="dark" variant="dark">
+        <Container>
+            <Navbar.Brand href="/"> 
+                <img
+                 src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+                 alt="logo"
+                 width="50"
+                 height="50"
+                 className="d-inline-block align-top"
+                 />
+            </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About us</Nav.Link>
+              <Nav.Link href="contacts" > Contacts</Nav.Link>
+              <Nav.Link href="/about">Blog</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      </>
+    );
+};
+
+export default Header;
