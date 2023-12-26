6ad6cce40e2d0c44d7db3017386d112f8daa48d1
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2023-12-26 13:47:41 +0300

    ed

--- src/page/Contacts.js
+++ src/page/Contacts.js
@@ -1,10 +1,35 @@
+//Компонент формы обратной связи
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
 
 const Contacts = () => {
     return (
-        <div>
-            <h1>Hello Contacts</h1>
-        </div>
+        <Container style={{width: '500px'}}>
+            <h1 className='text-center'>Contact us</h1>
+            <Form>
+                <Form.Group controlId='formBasicEmail' className='mb-3'>
+                    <Form.Label>
+                        Email adress
+                    </Form.Label>
+                    <Form.Control type='email' placeholder='Enter e-mail'/>
+                    <Form.Text>
+                        We'll never share your e-mail with anyone else!
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId='formBasicPassword' className='mb-3'>
+                    <Form.Label>
+                        Example textarea
+                    </Form.Label>
+                    <Form.Control as='textarea' rows='3'/>
+                </Form.Group>
+
+                <Form.Group controlId='formBasicCheckbox' className='mb-3'>
+                    <Form.Check type='checkbox' label='Check me out'/>
+                </Form.Group >
+                    <Button variant='outline-danger' type='submit'>Submit</Button>
+            </Form>
+        </Container>
     );
 };
 
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,9 +1,24 @@
+//Компонент со слайдером
 import React from 'react';
 import CarouselBox from '../components/CarouselBox';
+import { Card, CardGroup, Container } from 'react-bootstrap';
 
 const Home = () => {
     return (
+        <>
         <CarouselBox/>
+        <Container>
+                <h2 className='text-center m-4'>Popular film's</h2>
+                <CardGroup>
+                    <Card>
+                        <Card.Img
+                         variant='top'
+                         src='https://images.hdqwalls.com/download/spider-verse-miles-2020-hi-320x240.jpg'
+                         />
+                    </Card>
+                </CardGroup>
+        </Container>
+        </>
     );
 };
 
